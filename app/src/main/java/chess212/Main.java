5f57223f93a56cbf4dc2b72ca14d5f9f420c297a
kgb33 <keltonbassingthwaite@gmail.com>
2022-04-27 10:53:44 -0700

    Added checkstyle target.

--- app/src/main/java/chess212/Main.java
+++ app/src/main/java/chess212/Main.java
@@ -8,7 +8,7 @@ public class Main {
     return "Hello World!";
   }
 
-  public static void main(String[] args) {
+  public static void main(final String[] args) {
     System.out.println(new Main().getGreeting());
   }
 }
