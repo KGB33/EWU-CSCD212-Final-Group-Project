5f18c7baf6b4f18c66a67eb5a41fcb552d21c8a1
MowKitty <supermowkitty@gmail.com>
2022-05-25 10:18:02 -0700

    Arg Parsing Test

--- app/src/main/java/chess/Main.java
+++ app/src/main/java/chess/Main.java
@@ -1,11 +1,15 @@
 package chess;
 
+import core.classes.Board;
+import core.classes.Move;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.text.ParseException;
+import java.util.Scanner;
 
 public class Main extends Application {
   @Override
@@ -18,7 +22,36 @@ public class Main extends Application {
   }
 
   public static void main(String[] args) {
-    launch();
-  }
+    if (args.length >= 1 && args[0].equals("cli")) {
+      System.out.println("Starting cli... ");
+      Scanner kb = new Scanner(System.in);
+      Board b = new Board();
+      Move m;
+      String input;
+      do {
+        System.out.println(b.toString());
+        System.out.println(
+                "\n\nPlease enter a move in Algebraic Notation - including the from rank/file  ");
+        input = kb.nextLine().trim();
+        if (input.equals("q")) {
+          break;
+        }
+        try {
+          m = Move.parse(input);
+        } catch (ParseException e) {
+          System.out.println("Could not parse notation, please try again (q to quit).\n");
+          continue;
+        }
+        if (!b.move(m)) {
+          System.out.println("Could not move the peice, please try again (q to quit).\n");
+        }
 
+      } while (true);
+      kb.close();
+    }
+    else {
+      System.out.println("Starting GUI...");
+      launch();
+    }
+  }
 }
