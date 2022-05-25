dd65a59661205ee58f3b7ba3dbbe2346a587a859
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2022-05-25 12:59:15 -0700

    wrote pawn tests.

--- app/src/test/java/core/classes/PawnTest.java
+++ app/src/test/java/core/classes/PawnTest.java
@@ -3,7 +3,6 @@ package core.classes;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import core.enums.Color;
 import java.text.ParseException;
@@ -25,57 +24,84 @@ public class PawnTest {
     assertEquals("♙", k.toString());
   }
 
+  /**
+   * For En-passant to be a valid move.
+   *
+   * <ul>
+   *   <li>The capturing pawn must have advanced exactly three ranks to perform this move.
+   *   <li>The captured pawn must have moved two squares in one move, landing right next to the
+   *       capturing pawn.
+   *   <li>The en passant capture must be performed on the turn immediately after the pawn being
+   *       captured moves.
+   * </ul>
+   *
+   * <p>From: https://www.chess.com/terms/en-passant
+   */
   @Test
-  void testEnPassant() {
-    fail();
+  void testEnPassant() throws ParseException {
+    Board b = Board.createEmtpyBoard();
+    Pawn p = new Pawn(Color.WHITE);
+    b.setSquare('d', '7', new Pawn(Color.BLACK));
+    // Capturing pawn has advanced 3 ranks (2 -> 5)
+    b.setSquare('e', '5', p);
+    // Captured pawn advanced two squares in one
+    // move, landing next to the capturing pawn
+    b.move(Move.parse("d7d5"));
+    // Next move is en-passant
+    assertTrue(p.isValidMove(b, Move.parse("e5xd6")));
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"foo"})
+  @ParameterizedTest(name = "isValidMove (valid) {0}")
+  @ValueSource(strings = {"a2a4", "a2a3", "a2xb3", "c4c5"})
   void testIsValidMove(String notationIn) throws ParseException {
-    fail();
+    Board b = Board.createEmtpyBoard();
+    b.setSquare('a', '3', new Pawn(Color.BLACK));
+    Pawn p = new Pawn(Color.WHITE);
+    assertTrue(p.isValidMove(b, Move.parse(notationIn)));
   }
 
-  @ParameterizedTest
-  @ValueSource(strings = {"bar"})
+  @ParameterizedTest(name = "isValidMove (invalid) {0}")
+  @ValueSource(strings = {"a4a2", "a3a5", "a4a3", "cdxe5"})
   void testIsInValidMove(String notationIn) throws ParseException {
-    fail();
+    Board b = Board.createEmtpyBoard();
+    b.setSquare('e', '5', new Pawn(Color.BLACK));
+    Pawn p = new Pawn(Color.WHITE);
+    assertFalse(p.isValidMove(b, Move.parse(notationIn)));
   }
 
   @Test
   void testCannotJumpOverPeicesOfTheSameColor() throws ParseException {
     Board b = Board.createEmtpyBoard();
-    b.setSquare('a', '4', new Pawn(Color.WHITE));
-    Move m = Move.parse("Qa1a8");
-    Pawn q = new Pawn(Color.WHITE);
-    assertFalse(q.isValidMove(b, m));
+    b.setSquare('a', '3', new Pawn(Color.WHITE));
+    Move m = Move.parse("a2a4");
+    Pawn p = new Pawn(Color.WHITE);
+    assertFalse(p.isValidMove(b, m));
   }
 
   @Test
   void testCannotJumpOverPeicesOfDiffenetColors() throws ParseException {
     Board b = Board.createEmtpyBoard();
-    b.setSquare('a', '4', new Pawn(Color.BLACK));
-    b.setSquare('a', '1', new Pawn(Color.WHITE));
-    Move m = Move.parse("Qa1a8");
-    Pawn q = new Pawn(Color.WHITE);
-    assertFalse(q.isValidMove(b, m));
+    b.setSquare('a', '3', new Pawn(Color.BLACK));
+    Move m = Move.parse("a2a4");
+    Pawn p = new Pawn(Color.WHITE);
+    assertFalse(p.isValidMove(b, m));
   }
 
   @Test
   void testCannotCaputreSameTeam() throws ParseException {
     Board b = Board.createEmtpyBoard();
     b.setSquare('a', '4', new Pawn(Color.WHITE));
-    Move m = Move.parse("Qa1xa4");
-    Pawn q = new Pawn(Color.WHITE);
-    assertFalse(q.isValidMove(b, m));
+    Move m = Move.parse("b3xa4");
+    Pawn p = new Pawn(Color.WHITE);
+    assertFalse(p.isValidMove(b, m));
   }
 
   @Test
   void testCaptureOpposingPieces() throws ParseException {
     Board b = Board.createEmtpyBoard();
     b.setSquare('a', '4', new Pawn(Color.BLACK));
-    Move m = Move.parse("Qa1xa4");
-    Pawn q = new Pawn(Color.WHITE);
-    assertTrue(q.isValidMove(b, m));
+    Move m = Move.parse("b3xa4");
+    Pawn p = new Pawn(Color.WHITE);
+    assertTrue(p.isValidMove(b, m));
   }
 }
