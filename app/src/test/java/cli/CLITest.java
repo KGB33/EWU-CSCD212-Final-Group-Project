1ddc4629918ce9eff176c692b04180ad32615c62
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2022-06-02 15:12:53 -0700

    fixed path handleing for the expected result data

--- app/src/test/java/cli/CLITest.java
+++ app/src/test/java/cli/CLITest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
@@ -13,15 +14,14 @@ import org.junit.jupiter.api.Test;
 class CLITest {
 
   @Test
-  void TestFoolsMate() throws IOException {
+  void TestFoolsMate() throws IOException, URISyntaxException {
     Scanner moves = new Scanner("f2f3\ne7e6\ng2g4\nQd8h4\na2a4\nQh4xe1");
     ByteArrayOutputStream outputContent = new ByteArrayOutputStream();
     PrintStream output = new PrintStream(outputContent);
     CLI.run(moves, output);
 
-    ClassLoader classLoader = getClass().getClassLoader();
     String expected =
-        Files.readString(Path.of(classLoader.getResource("cli/expected/FoolsMate.txt").getPath()));
+        Files.readString(Path.of(CLITest.class.getResource("/cli/expected/FoolsMate.txt").toURI()));
     assertEquals(expected.toString(), outputContent.toString());
   }
 }
