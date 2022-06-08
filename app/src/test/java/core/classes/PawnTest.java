035ed1ab5a381cf7f32e054d2221dbc99f1a848c
Kelton Bassingthwaite <KeltonBassingthwaite@gmail.com>
2022-06-07 19:51:20 -0700

    added pawn score test.

--- app/src/test/java/core/classes/PawnTest.java
+++ app/src/test/java/core/classes/PawnTest.java
@@ -104,4 +104,10 @@ public class PawnTest {
     Pawn p = new Pawn(Color.WHITE);
     assertTrue(p.isValidMove(b, m));
   }
+
+  @Test
+  void testGetScore() {
+    final Pawn p = new Pawn(Color.WHITE);
+    assertEquals(10, p.getScore());
+  }
 }
